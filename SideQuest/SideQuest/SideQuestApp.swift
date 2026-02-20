b52440d59b9bd52f7bb41effd6a0c657f75ac7c3
Karson Mellott <karsonmellott@gmail.com>
2026-02-20 07:55:15 -0700

    Added Project

---
+++ SideQuest/SideQuest/SideQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideQuestApp.swift
+//  SideQuest
+//
+//  Created by Karson Mellott on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SideQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
